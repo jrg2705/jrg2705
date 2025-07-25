9d973f0663aa33b7102d00269471da935c2965d1
jrg2705 <jrgonzalez.2705@gmail.com>
2025-07-25 15:38:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -318,4 +318,4 @@ def logout():
     return redirect(url_for('inicio'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=False)
+    app.run(host='0.0.0.0', port=5001, debug=True)
